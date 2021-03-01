ce2b1f5af15a56ce95747d52c1412e6fbb6ae08a
immanual19 <immanuelsarker1@gmail.com>
2021-03-01 13:12:46 +0600

    third-commit

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,11 +1,20 @@
 import React from 'react';
 
 const Cart = (props) => {
+   // console.log(props.playersCart);
+   const playersCart=props.playersCart;
+   console.log(playersCart);
+   let totalPrice=0;
+   for(let i=0;i<playersCart.length;i++){
+       const player=playersCart[i];
+       totalPrice=totalPrice+player.base_price;
+   }
     return (
         <div>
         <div>
             <h2>Team Summary</h2>
             <h3>Player selected: {props.playersCart.length}</h3>
+            <h3>Total Price: $ {totalPrice}</h3>
         </div>
 
         <div>
